bbb22911bab63dbe229e8317f429e0d74ed2eee2
DevYmA <ymamarathunga@gmail.com>
2017-10-19 11:25:20 +0530

    add few updates

---
+++ static/js/custome.js
@@ -0,0 +1,7 @@
+var section_1 = new Typed('#tel > span', {
+    strings: ['Hello !', 'I am Yoshan Amarathunga' ,'I am working as a<strong> Full Stack Developer</strong>'],
+    typeSpeed: 70,
+    backSpeed: 70,
+    smartBackspace: true,
+    loop: true
+});
